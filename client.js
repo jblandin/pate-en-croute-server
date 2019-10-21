50c6f58f063c77238bdb105e5ed2c559b45cf428
Julien Blandineau <jblandineau@gmail.com>
2019-10-22 00:48:53 +0200

    Ajout client.js

---
+++ client.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+const _port = 80;
+const _app_folder = '../pate-en-croute-client/dist/fp-affichage';
+
+
+
+// ---- SERVE STATIC FILES ---- //
+app.get('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+// ---- SERVE APLICATION PATHS ---- //
+app.all('*', function (req, res) {
+    res.status(200).sendFile(`/`, {root: _app_folder});
+});
+
+// ---- ENABLE CORS FOR ALL ORIGINS ---- //
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+// ---- START UP THE NODE SERVER  ----
+app.listen(_port, function () {
+    console.log("Node Express server for " + app.name + " listening on http://localhost:" + _port);
+});
